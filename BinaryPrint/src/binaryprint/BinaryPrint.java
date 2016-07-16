e93607b33978981a4047ddac2c5e662d7fe93970
deanzel <deanchoi@gmail.com>
2016-07-16 16:20:00 -0400

    HW Assignment #2 started with Binary Search

---
+++ BinaryPrint/src/binaryprint/BinaryPrint.java
@@ -0,0 +1,19 @@
+/*
+Name: Dean Choi
+CSU ID: 2690159
+CIS 265: Assignment #2
+Description: Recursive method that prints the binary form a given non-negative integer
+*/
+package binaryprint;
+
+
+public class BinaryPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ BinaryPrint/src/binaryprint/nPrint.java
@@ -0,0 +1,12 @@
+/*
+Name: Dean Choi
+CSU ID: 2690159
+CIS 265: Assignment #2
+Description: Recursive method that prints the binary form a given non-negative integer
+*/
+package binaryprint;
+
+
+public class nPrint {
+    
+}
